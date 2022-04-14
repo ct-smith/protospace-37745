eb5db662f0502e0c61c1686a0ff813a1a8e9c2f5
ct-smith <melple0212@gmail.com>
2022-04-14 17:18:09 +0900

    編集ページへのアクセス制限の実装

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,6 @@
 class PrototypesController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit, :destroy]
+  before_action :move_to_index, only: [:edit]
 
   def index
     @prototypes = Prototype.all
@@ -26,9 +27,6 @@ class PrototypesController < ApplicationController
 
   def edit
     @prototype = Prototype.find(params[:id])
-    unless user_signed_in?
-      redirect_to :root_path
-    end
   end
 
   def update
@@ -50,6 +48,13 @@ class PrototypesController < ApplicationController
 
    def prototype_params
       params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+   end
+
+   def move_to_index
+    @prototype = Prototype.find(params[:id])
+     unless user_signed_in? && current_user.id == @prototype.user_id
+        redirect_to root_path
+      end
    end
 
 
